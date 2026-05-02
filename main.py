1fd6ebd0e5952db091c46a9196246d505cce8348
chaos5starz <chaos5starz@gmail.com>
2026-05-02 22:54:36 +0300

    Add Gradio UI for AI chatbot

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from langchain.agents import create_agent
 from langchain_ollama import ChatOllama
 from datetime import datetime
-
+import gradio as gr
 
 load_dotenv()
 
@@ -13,14 +13,24 @@ def getdate():
 llm = ChatOllama(model="qwen2.5:3b")
 
 system_prompt = """
-you are a helpful assistant that can answer questions and perform tasks.
+you are a helpful assistant that can answer user questions,
 use the getdate function to get the current date."""
 
 agent = create_agent(model = llm ,tools = [getdate] , system_prompt = system_prompt)
-user_query = input("Enter a query: ")
-response = agent.invoke({"messages": [{"role": "user", "content": user_query}]})
 
-print(response["messages"][-1].content)
+
+def chat(user_query , history):
+    response = agent.invoke({"messages": [{"role": "user", "content": user_query}]})
+    last_respone = response["messages"][-1].content
+    return last_respone
+
+
+with gr.Blocks() as demo :
+    gr.Markdown("Ai chatbot")
+    gr.ChatInterface(fn = chat)
+
+    
+demo.launch()
 
 
 
